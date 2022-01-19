e8cc44a65e69c797970c46067b9b5e856a26af5e
Usta_Kozim <kozimjonomonjonov88@gmail.com>
2022-01-20 00:17:51 +0500

    header responsive

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function burgerButton() {
+  
+  // const elMenuList = document.querySelector(".menu__list").classList.toggle("menu-list__responsive");
+  const elBurger = document.querySelector(".burger-button").classList.toggle("hamburger");
+
+}
