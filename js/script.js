a749ab2b756e3ee90cb0017db94cc2fb87405b05
Usta_Kozim <kozimjonomonjonov88@gmail.com>
2022-01-20 15:57:13 +0500

    header and banner

--- js/script.js
+++ js/script.js
@@ -1,12 +1,37 @@
-function burgerButton() {
+// function burgerButton() {
 
-    // const elMenuList = document.querySelector(".menu__list").classList.toggle("menu-list__responsive");
-    const elBurger = document.querySelector(".burger-button").classList.toggle("hamburger");
+//     // const elMenuList = document.querySelector(".menu__list").classList.toggle("menu-list__responsive");
+//     const elBurger = document.querySelector(".burger-button").classList.toggle("hamburger");
 
+// }
+// let btn = document.querySelector("#burgerBtn");
+// let menu = document.querySelector("#navbar");
+// btn.addEventListener("click", () => {
+//     menu.classList.toggle("menu-list-active")
+//     menu.classList.toggle("menu-list")
+// })
+
+
+let num = 0;
+function controlMenu(){
+    num++
+function mediaNavbar(x) {
+    var growDiv = document.querySelector('.sc2_navbar_menu');
+    var wrapper = growDiv.querySelector('ul');
+  if (x.matches) { 
+          if(num >= 2){num = 0}
+          if(num == 0){growDiv.style.height = "0px";
+                       growDiv.style = "border-bottom:none;"}
+          if(num == 1){growDiv.style.height = wrapper.clientHeight + "px";
+                       growDiv.style.borderBottom = "1px solid #ffc000"
+                       }
+  }else {
+    growDiv.style = ""
+    num = 0
+  }
 }
-let btn = document.querySelector("#burgerBtn");
-let menu = document.querySelector("#navbar");
-btn.addEventListener("click", () => {
-    menu.classList.toggle("menu-list-active")
-    menu.classList.toggle("menu-list")
-})
+
+var x = window.matchMedia("(max-width: 992px)")
+mediaNavbar(x)
+x.addListener(mediaNavbar) 
+}
