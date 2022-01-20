3f724a9d63e6c43ec58f4bc49ecfea21b8a5a035
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-01-20 13:29:53 +0500

    update project

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 function burgerButton() {
-  
-  // const elMenuList = document.querySelector(".menu__list").classList.toggle("menu-list__responsive");
-  const elBurger = document.querySelector(".burger-button").classList.toggle("hamburger");
+
+    // const elMenuList = document.querySelector(".menu__list").classList.toggle("menu-list__responsive");
+    const elBurger = document.querySelector(".burger-button").classList.toggle("hamburger");
 
 }
+let btn = document.querySelector("#burgerBtn");
+let menu = document.querySelector("#navbar");
+btn.addEventListener("click", () => {
+    menu.classList.toggle("menu-list-active")
+    menu.classList.toggle("menu-list")
+})
